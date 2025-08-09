2757eafd3b264fce8ea7bd952470b89149fd1dfe
JenishSambahamphe@24 <limbujenish24@gmail.com>
2025-08-09 23:08:55 +0545

    prisma setup done project route created

---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import  express  from "express";
+import dotenv from 'dotenv';
+import bodyParser from "body-parser";
+import cors from 'cors'
+import morgan from "morgan";
+import helmet from "helmet";
+import projectRoutes from './routes/projectRoutes'
+
+// configuration
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('this is home route');
+});
+
+app.use('/projects', projectRoutes)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is running at ${port}`);
+});
